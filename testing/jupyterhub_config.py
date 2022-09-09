a96e576ab9fbe54d037b2bd6354590002abcb417
mhasan49 <mhasan49computation@gmail.com>
2022-09-09 14:45:58 +0200

    authenticator files

--- testing/jupyterhub_config.py
+++ testing/jupyterhub_config.py
@@ -2,21 +2,22 @@
 configures jupyterhub with dummyauthenticator and simplespawner
 to enable testing without administrative privileges.
 """
-
 import netifaces
 import os
 import sys
 from dockerspawner import DockerSpawner
 from jupyterhub.auth import DummyAuthenticator
-from my_script import my_banner
 
 script_dir = os.path.dirname(__file__)
 file_path = os.path.join(script_dir, '..', 'testing')
 sys.path.append(file_path)
+
+from my_script import my_banner
+
 custom_text = my_banner("jupyterHub")
 custom_text.print_banner()
 
-c = get_config()
+c = get_config()  # noqa
 
 c.JupyterHub.authenticator_class = DummyAuthenticator
 
@@ -26,6 +27,9 @@ c.Authenticator.admin_users = {'admin1', 'admin2', 'admin3'}
 c.DummyAuthenticator.password = "admin12345"
 c.LocalAuthenticator.create_system_users = True
 c.Authenticator.delete_invalid_users = True
+
+
+
 # Optionally set a global password that all users must use
 # c.DummyAuthenticator.password = "your_password"
 
@@ -48,6 +52,7 @@ $ newgrp docker  # Start new shell where docker group is activated
 """
 
 c.JupyterHub.spawner_class = DockerSpawner
+
 docker_iface = netifaces.ifaddresses('docker0')
 c.JupyterHub.hub_ip = docker_iface[netifaces.AF_INET][0]['addr']
 c.DockerSpawner.cpu_guarantee = 0.5
@@ -57,7 +62,3 @@ c.DockerSpawner.cpu_guarantee = 0.5
 # c.JupyterHub.spawner_class = SimpleLocalProcessSpawner
 
 from jupyterhub.spawner import Spawner, LocalProcessSpawner
-
-c.DockerSpawner.image = 'jupyterhub/singleuser'
-c.DockerSpawner.network_name = 'jupyterhub'
-
