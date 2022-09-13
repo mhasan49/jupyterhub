f41ce3b7fadddd72013325ff97286db979d0197d
mhasan49 <mhasan49computation@gmail.com>
2022-09-13 22:32:10 +0200

    custom spawner option form

--- testing/test_config.py
+++ testing/test_config.py
@@ -62,25 +62,6 @@ class DemoFormSpawner(LocalProcessSpawner):
         return options
 
 
-
-
-   # def get_args(self):
-   #     """Return arguments to pass to the notebook server"""
-   #     argv = super().get_args()
-   #     if self.user_options.get('argv'):
-   #         argv.extend(self.user_options['argv'])
-   #     return argv
-
-   # @property
-   # def get_env(self):
-   #     env = super().get_env()
-   #     if self.user_options.get('env'):
-    #        env.update(self.user_options['env'])
-    #    return env
-
-
-c.JupyterHub.spawner_class = DemoFormSpawner
-
 c = get_config()  # noqa
 c.JupyterHub.authenticator_class = DummyAuthenticator
 c.JupyterHub.admin_access = True                            # allow admin users to access single-user servers
